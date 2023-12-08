d5d8398f000818ae7f497f5d1b7b065c1751775b
EleniPP <eleni1997pp@gmail.com>
2023-12-08 19:04:04 +0200

    Deaths chart

--- preprocessing_deaths.py
+++ preprocessing_deaths.py
@@ -14,9 +14,38 @@ for year in years:
     total_years.append(total)
     total = []
 
+total_rev = []
+total_years_rev = []
+for disease in diseases:
+    for year in years:
+        df_yr = df[df['Year'] == year]
+        total_rev.append(df_yr[disease].sum())
+    total_years_rev.append(total_rev)
+    total_rev = []
+
+total_dis = []
+for disease in diseases:
+    total_dis.append(df[disease].sum())
+
+
 new_df = pd.DataFrame()
 new_df = pd.DataFrame.from_records(total_years, columns=diseases)
 new_df['Year'] = years
 
+rev_df = pd.DataFrame()
+rev_df = pd.DataFrame.from_records(total_years_rev, columns=years)
+rev_df['Diseases'] = diseases
+
+dis_df = pd.DataFrame()
+dis_df['Deaths'] = total_dis
+dis_df['Diseases'] = diseases 
+dis_df.sort_values(dis_df.columns[0],inplace=True, ascending=False)
+
+# os.makedirs('./', exist_ok=True)
+# new_df.to_csv('./out.csv', index=False)
+
+# os.makedirs('./', exist_ok=True)
+# rev_df.to_csv('./out_rev.csv', index=False)
+
 os.makedirs('./', exist_ok=True)
-new_df.to_csv('./out.csv', index=False)
+dis_df.to_csv('./out_dis.csv', index=False)
