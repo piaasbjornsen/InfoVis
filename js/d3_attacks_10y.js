42e755c8a1fd832ce6f0c94b7a4d12933806630f
EleniPP <eleni1997pp@gmail.com>
2023-12-21 19:28:33 +0200

    update-attacks-circular graph-file

--- js/d3_attacks_10y.js
+++ js/d3_attacks_10y.js
@@ -1,84 +1,91 @@
-var containerDivCasualties = d3.select("#cause_of_death_div");
+// set the dimensions and margins of the graph
+var margin = {top: 100, right: 60, bottom: 60, left: 60},
+    width = 660 - margin.left - margin.right,
+    height = 660 - margin.top - margin.bottom,
+    innerRadius =90,
+    outerRadius = Math.min(width, height) / 2;   // the outerRadius goes from the middle of the SVG area to the border
 
-  var margin = { top: 50, right: 50, bottom: 190, left: 80 },
-    width = containerDivCasualties.node().getBoundingClientRect().width - margin.left - margin.right,
-    height = 700 - margin.top - margin.bottom;
-
-  // append the svg object to the body of the page
-  var svg = containerDivCasualties
-    .append("svg")
+// append the svg object
+var svg = d3.select("attacks-10yr")
+  .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  .append("g")
+    .attr("transform", "translate(" + (width / 2 + margin.left) + "," + (height / 2 + margin.top) + ")");
 
-  // Parse the Data
-  d3.csv("assets/csv/out_dis.csv").then(function (data) {
-    // X axis
-    var x = d3.scaleBand()
-      .range([0, width])
-      .domain(data.map(function (d) { return d.Diseases; }))
-      .padding(0.2);
+d3.csv("assets/csv/attacks_by_country_10y.csv", function(data) {
 
-    svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x))
-      .selectAll("text")
-      .attr("transform", "translate(-10,0)rotate(-45)")
-      .style("text-anchor", "end");
+  // Scales
+  var x = d3.scaleBand()
+      .range([0, 2 * Math.PI])    // X axis goes from 0 to 2pi = all around the circle. If I stop at 1Pi, it will be around a half circle
+      .align(0)                  // This does nothing
+      .domain(data.map(function(d) { return d.Country; })); // The domain of the X axis is the list of states.
+  var y = d3.scaleRadial()
+      .range([innerRadius, outerRadius])   // Domain will be define later.
+      .domain([0, 14000]); // Domain of Y is from 0 to the max seen in the data
 
-    // Add Y axis
-    var y = d3.scaleLinear()
-      .domain([0, 2700000000])
-      .range([height, 0]);
-    svg.append("g")
-      .call(d3.axisLeft(y));
+  // Build color scale
+  var myColor = d3.scaleSequential()
+  .interpolator(d3.interpolateInferno)
+  .domain([20995, 2494])
+  // create a tooltip
+var Tooltip = d3.select("#attacks-10yr")
+.append("div")
+.style("opacity", 0)
+.attr("class", "tooltip")
+.style("background-color", "white")
+.style("border", "solid")
+.style("border-width", "2px")
+.style("border-radius", "5px")
+.style("padding", "5px")
+.style("position", "absolute")
 
-    // Interactive bar
-    // create a tooltip
-    var Tooltip = containerDivCasualties
-      .append("div")
-      .style("opacity", 0)
-      .attr("class", "tooltip")
-      .style("background-color", "white")
-      .style("border", "solid")
-      .style("border-width", "2px")
-      .style("border-radius", "5px")
-      .style("padding", "5px");
+// Three function that change the tooltip when user hover / move / leave a cell
+var mouseover = function(d) {
+Tooltip
+    .style("opacity", 1)
+}
+var mousemove = function(d) {
+Tooltip
+    .html("Number of incidents: " + d['Attack_Count'])
+    .style("left", (d3.mouse(this)[0]+100) + "px")
+    .style("top", (d3.mouse(this)[1]+350) + "px")
+}
+var mouseleave = function(d) {
+Tooltip
+    .style("opacity", 0)
+}
 
-    // Three function that change the tooltip when user hover / move / leave a cell
-    var mouseover = function (d) {
-      Tooltip.style("opacity", 1);
-    };
-    var mousemove = function (d) {
-      Tooltip.html("Number of deaths: " + d.Deaths)
-        .style("left", (d3.mouse(this)[0] + 70) + "px")
-        .style("top", (d3.mouse(this)[1]) + "px");
-    };
-    var mouseleave = function (d) {
-      Tooltip.style("opacity", 0);
-    };
-
-    // Bars
-    svg.selectAll("mybar")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr("class", "myCircle")
-      .attr("x", function (d) { return x(d.Diseases); })
-      .attr("width", x.bandwidth())
-      .attr("fill", "#69b3a2")
+  // Add the bars
+  svg.append("g")
+    .selectAll("path")
+    .data(data)
+    .enter()
+    .append("path")
+      .attr("fill",  function(d) { return myColor(d['Attack_Count'])})
+      // .attr("fill",  "heatmap")
+      .attr("d", d3.arc()     // imagine your doing a part of a donut plot
+          .innerRadius(innerRadius)
+          .outerRadius(function(d) { return y(d['Attack_Count']); })
+          .startAngle(function(d) { return x(d.Country); })
+          .endAngle(function(d) { return x(d.Country) + x.bandwidth(); })
+          .padAngle(0.01)
+          .padRadius(innerRadius))
       .on("mouseover", mouseover)
       .on("mousemove", mousemove)
       .on("mouseleave", mouseleave)
-      .attr("height", function (d) { return height - y(0); }) // always equal to 0
-      .attr("y", function (d) { return y(0); });
+  // Add the labels
+  svg.append("g")
+      .selectAll("g")
+      .data(data)
+      .enter()
+      .append("g")
+        .attr("text-anchor", function(d) { return (x(d.Country) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "end" : "start"; })
+        .attr("transform", function(d) { return "rotate(" + ((x(d.Country) + x.bandwidth() / 2) * 180 / Math.PI - 90) + ")"+"translate(" + (y(d['Attack_Count'])+10) + ",0)"; })
+      .append("text")
+        .text(function(d){return(d.Country)})
+        .attr("transform", function(d) { return (x(d.Country) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "rotate(180)" : "rotate(0)"; })
+        .style("font-size", "11px")
+        .attr("alignment-baseline", "middle")
 
-    // Animation
-    svg.selectAll("rect")
-      .transition()
-      .duration(800)
-      .attr("y", function (d) { return y(d.Deaths); })
-      .attr("height", function (d) { return height - y(d.Deaths); })
-      .delay(function (d, i) { return (i * 100); });
-  });
+});
